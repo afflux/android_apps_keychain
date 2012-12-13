d98bbc87c5fd03f44ca2e1f2fd01fd5e8258dc71
Kjell Braden <afflux@pentabarf.de>
2012-12-13 10:48:37 +0100

    move generate dialog into CHOOSER dialog, fix grant checking, implement new api from afflux/android_frameworks_base@f84b66315f8bb11975d72b16dafea16fbd94c00b

--- src/com/android/keychain/CryptOracleService.java
+++ src/com/android/keychain/CryptOracleService.java
@@ -1,11 +1,12 @@
 
 package com.android.keychain;
 
-import android.app.IntentService;
+import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.security.Credentials;
+import android.security.CryptOracle.StringAliasNotFoundException;
 import android.security.ICryptOracleService;
 import android.security.KeyChain;
 import android.security.KeyStore;
@@ -19,6 +20,7 @@ import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.Signature;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
@@ -33,11 +35,32 @@ import javax.crypto.NoSuchPaddingException;
 /**
  * @author Kjell Braden <kjell.braden@stud.tu-darmstadt.de>
  */
-public class CryptOracleService extends IntentService {
+public class CryptOracleService extends Service {
     private static final String TAG = "KeyChain";
 
     private final ICryptOracleService.Stub mICryptOracleService = new ICryptOracleService.Stub() {
         private final KeyStore mKeyStore = KeyStore.getInstance();
+
+        private boolean checkGrant(String alias) throws RemoteException {
+            final int uid = getCallingUid();
+            long token = clearCallingIdentity();
+
+            Log.d(TAG, "checking grant for " + uid + " to " + alias);
+
+            KeyChain.KeyChainConnection connection = null;
+            try {
+                connection = KeyChain.bind(CryptOracleService.this);
+                return connection.getService().hasGrant(uid, alias);
+            } catch (InterruptedException ignored) {
+                Thread.currentThread().interrupt();
+                Log.d(TAG, "interrupted while granting access", ignored);
+                return false;
+            } finally {
+                if (connection != null)
+                    connection.close();
+                restoreCallingIdentity(token);
+            }
+        }
 
         @Override
         public byte[] decryptData(String alias, String padding, byte[] encryptedData)
@@ -69,7 +92,8 @@ public class CryptOracleService extends IntentService {
                 throws NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
 
             Log.d(TAG,
-                    MessageFormat.format("doCrypt({0}, keyformat={1}, padding={2}, datalen={3})",
+                    MessageFormat.format(
+                            "doCrypt({0}, keyformat={1}, padding={2}, datalen={3})",
                             mode,
                             key.getAlgorithm(), padding, data.length));
             String algorithm = key.getAlgorithm();
@@ -102,30 +126,25 @@ public class CryptOracleService extends IntentService {
         public byte[] encryptData(String alias, String padding, byte[] plainData)
                 throws RemoteException {
 
-            PublicKey pubKey = getPubKey(alias);
-
             try {
+                PublicKey pubKey = getPubCert(alias).getPublicKey();
+
                 return doCrypt(Cipher.ENCRYPT_MODE, pubKey, padding, plainData);
             } catch (GeneralSecurityException e) {
+                throw suppressedRemoteException(e);
+            } catch (RuntimeException e) {
                 throw suppressedRemoteException(e);
             }
         }
 
         private PrivateKey getPrivKey(String alias) throws RemoteException {
-            final int callingUid = getCallingUid();
-
-            Log.d(TAG, "checking grant for " + callingUid + " to " + alias);
-            try {
-                KeyChain.KeyChainConnection connection = KeyChain.bind(CryptOracleService.this);
-                if (!connection.getService().hasGrant(callingUid, alias))
-                    throw suppressedRemoteException(new IllegalStateException("uid " + callingUid
-                            + " doesn't have permission to access the requested alias"));
-            } catch (InterruptedException ignored) {
-                Thread.currentThread().interrupt();
-                Log.d(TAG, "interrupted while granting access", ignored);
-            }
+            if (!checkGrant(alias))
+                throw suppressedRemoteException(new StringAliasNotFoundException());
 
             byte[] encodedKey = this.mKeyStore.get(Credentials.USER_PRIVATE_KEY + alias);
+            if (encodedKey == null)
+                throw suppressedRemoteException(new StringAliasNotFoundException());
+
             try {
                 return KeyFactory.getInstance("RSA").generatePrivate(
                         new PKCS8EncodedKeySpec(encodedKey));
@@ -143,16 +162,61 @@ public class CryptOracleService extends IntentService {
             // }
         }
 
-        private PublicKey getPubKey(String alias) throws RemoteException {
+        private Certificate getPubCert(String alias) throws RemoteException {
+            if (!checkGrant(alias))
+                throw suppressedRemoteException(new StringAliasNotFoundException());
+
             byte[] byteCert = this.mKeyStore.get(Credentials.USER_CERTIFICATE + alias);
+            if (byteCert == null)
+                throw suppressedRemoteException(new StringAliasNotFoundException());
+
             try {
                 CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
                 Certificate cert = certFactory.generateCertificate(new ByteArrayInputStream(
                         byteCert));
-                return cert.getPublicKey();
+                return cert;
             } catch (CertificateException e) {
                 throw suppressedRemoteException(e);
             }
+        }
+
+        private void setGrant(String alias) throws RemoteException {
+            final int uid = getCallingUid();
+            long token = clearCallingIdentity();
+
+            Log.d(TAG, "setting grant for " + uid + " to " + alias);
+
+            KeyChain.KeyChainConnection connection = null;
+            try {
+                connection = KeyChain.bind(CryptOracleService.this);
+                connection.getService().setGrant(uid, alias, true);
+            } catch (InterruptedException ignored) {
+                Thread.currentThread().interrupt();
+                Log.d(TAG, "interrupted while granting access", ignored);
+            } finally {
+                if (connection != null)
+                    connection.close();
+                restoreCallingIdentity(token);
+            }
+        }
+
+        @Override
+        public byte[] sign(String alias, String algorithm, byte[] data) throws RemoteException {
+            try {
+                Signature sig = Signature.getInstance(algorithm);
+                sig.initSign(getPrivKey(alias));
+                sig.update(data);
+                return sig.sign();
+            } catch (GeneralSecurityException e) {
+                throw suppressedRemoteException(e);
+            }
+        }
+
+        @Override
+        public void storePublicCertificate(String alias, byte[] pemEncodedCert)
+                throws RemoteException {
+            setGrant(alias);
+            this.mKeyStore.put(Credentials.USER_CERTIFICATE + alias, pemEncodedCert);
         }
 
         private RemoteException suppressedRemoteException(Throwable e) {
@@ -161,18 +225,23 @@ public class CryptOracleService extends IntentService {
             re.addSuppressed(e);
             return re;
         }
-    };
 
-    public CryptOracleService() {
-        super(CryptOracleService.class.getSimpleName());
-    }
+        @Override
+        public boolean verify(String alias, String algorithm, byte[] data, byte[] signature)
+                throws RemoteException {
+            try {
+                Signature sig = Signature.getInstance(algorithm);
+                sig.initVerify(getPubCert(alias));
+                sig.update(data);
+                return sig.verify(signature);
+            } catch (GeneralSecurityException e) {
+                throw suppressedRemoteException(e);
+            }
+        }
+    };
 
     @Override
     public IBinder onBind(Intent intent) {
         return this.mICryptOracleService;
-    }
-
-    @Override
-    protected void onHandleIntent(Intent intent) {
     }
 }
